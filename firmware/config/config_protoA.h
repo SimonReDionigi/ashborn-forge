66eab7d467b536c95ce47e3728771f534b3c28a7
RiccardoCanalipv <riccardo.canali1@outlook.com>
2025-08-01 16:45:50 +0200

    Update config_protoA.h
    Added:
    - ADS1118 features
    Changed:
    - switched MISO and MOSI resource
    - Pins name for greater clarity
    - Cell Case temperature Pin switched from A4 to A3

--- firmware/config/config_protoA.h
+++ firmware/config/config_protoA.h
@@ -17,45 +17,45 @@
 
     //pin mapping
     //SPI
-    #define CLK 13
-    #define MOSI 12
-    #define MISO 11
-    #define CS_VOLT 10 //chip select for voltage sensor adc
-    #define CS_CURR_CHRG 9 //chip select for charging current adc
-    #define CS_CURR_DISCH 8 //chip select for discharging current adc
+    #define PIN_CLK 13
+    #define PIN_MISO 12
+    #define PIN_MOSI 11
+    #define PIN_CS_VOLT 10 //chip select for voltage sensor adc
+    #define PIN_CS_CURR_CHRG 9 //chip select for charging current adc
+    #define PIN_CS_CURR_DISCH 8 //chip select for discharging current adc
     //Mosfet
-    #define GATE_DISCH 6 //gate mosfet for discharge control
-    #define GATE_CHRG 5 //gate mosfet for charge control
-    #define GATE_FAN 4 //gate mosfet for fans control
-    //Temeperature
-    #define TEMP_CELL A0 //Cell temperature
-    #define TEMP_CELL_CASE A4 //Cell case temperature
-    #define TEMP_RES A2 //Resistor
+    #define PIN_GATE_DISCH 6 //gate mosfet for discharge control
+    #define PIN_GATE_CHRG 5 //gate mosfet for charge control
+    #define PIN_GATE_FAN 4 //gate mosfet for fans control
+    //Temperature
+    #define PIN_TEMP_CELL A0 //Cell temperature
+    #define PIN_TEMP_CELL_CASE A3 //Cell case temperature
+    #define PIN_TEMP_RESISTORS A2 //resistors temperature
 
-    //HW pararameters
+    //HW parameters
     //Resistors
     #define LOAD_RES 1 //load resistance of the discharge circuit
     #define GAIN_RES 10 //gain resistance for all mosfet
     #define GS_RES 10000 // gain-source resistance for all mosfet
-    #define TEMP_RES 10000 //resistance of resistor in series on the voltage devider for NTC
+    #define TEMP_RES 10000 //resistance of resistor in series on the voltage divider for NTC
     #define VSENS_FILT_RES 10000 //resistance of the voltage sensing filter
     #define ISENS_DISCH_FILT_RES 470 //resistance of the discharge current sensing filter
     #define ISENS_CHRG_FILT_RES 470 //resistance of the charge current sensing filter
     //Capacitors
-    #define CAP_VSENSE_PRT "B32653A4105J000" //part number of the votlage filter capacitor
+    #define CAP_VSENSE_PRT "B32653A4105J000" //part number of the voltage filter capacitor
     #define CAP_ISENSE_DISCH_PRT "B32653A4105J000" //part number of the discharge current filter capacitor
     #define CAP_ISENSE_CHRG_PRT "B32653A4105J000" //part number of the charge current filter capacitor
-    #define VSENS_FILT_CAP 1e-6 //capacitance of the votlage sensing filter
-    #define ISENS_DISCH_FILT_CAP 1e-6 //capacitance of the discharge current sensing filter
-    #define ISENS_CHRG_FILT_CAP 1e-6 //capacitance of the charge current sensing filer
+    #define CAP_VSENS_FILT 1e-6 //capacitance of the voltage sensing filter
+    #define CAP_ISENS_DISCH_FILT 1e-6 //capacitance of the discharge current sensing filter
+    #define CAP_ISENS_CHRG_FILT 1e-6 //capacitance of the charge current sensing filter
     //Temperature sensor
     #define NTC_PRT "B57861S0103F045" //part number of the NTC
     #define NTC_NOM_RES 10000 //nominal resistance at 25°C of the NTC sensor
-    #define NTC_B25_100 3988 //Beta constante of the NTC
+    #define NTC_B25_100 3988 //Beta constant of the NTC
     #define NTC_NOM_TEMP 25.0 //nominal NTC temperature in °C
-    #define NTC_TOLL_RES 0.01 //tollerance of the NTC resistance
-    #define NTC_TOLL_B 0.003 //tollerance of the NTC B value
-    #define NTC_VREF 5 //reference voltage of the NTC voltage devider 
+    #define NTC_TOLL_RES 0.01 //tolerance of the NTC resistance
+    #define NTC_TOLL_B 0.003 //tolerance of the NTC B value
+    #define NTC_VREF 5 //reference voltage of the NTC voltage divider 
     //Mosfet
     #define MOS_DISCH_PRT "IRF3205PBF" //Discharge Mosfet Part Number
     #define MOS_CHRG_PRT "IRF3205PBF" //Charge Mosfet Part Number
@@ -78,9 +78,12 @@
     #define CUR_SENS_DISCH_VREF 5 //Discharge current sensor voltage reference
     #define CUR_SENS_CHRG_VREF 5 //Discharge current sensor voltage reference
     //adc
-    #define ARDUINO_ADC_BIT 12 //number of bit for Arduino ADC
-    #define ARDUINO_ADC_MAX_VAL 4095.0 //maximum value for arduino ADC
-
-
+    #define ARDUINO_ADC_BIT 12 //number of bits for Arduino ADC
+    #define ARDUINO_ADC_MAX_VAL 4095 //maximum value for arduino ADC
+    #define ADC_PRT "ADS1118" //External ADC part number used for current and voltage acquisition 
+    #define ADC_BIT 16 //number of bits of external ADC
+    #define ADC_MAX_VAL 65535 //maximum value of external ADC
+    #define ADC_VREF 4.096 //External ADC internal reference voltage 
+    #define ADC_GAIN 1 //External ADC gain
 
 #endif
