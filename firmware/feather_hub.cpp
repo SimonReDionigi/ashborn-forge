0cc437a5667b1d2f942f34b0c7cc2437932e3741
RiccardoCanalipv <riccardo.canali1@outlook.com>
2025-08-05 08:34:08 +0200

    Comment out .cpp includes to fix compilation errors
    Commented out direct inclusion of .cpp files in feather_hub.cpp to avoid compilation errors. This change was made to ensure only header files are included, following standard C++ practices.

--- firmware/feather_hub.cpp
+++ firmware/feather_hub.cpp
@@ -3,6 +3,8 @@
  * Description: Main file for the Feather Hub project, integrating various components
  * Author: Phoenix Volt
  * Created: 2025-08-04
+ * Last Modified: 
+ * - 2025-08-05 by Riccardo Canali - Commented .cpp files to avoid compilation errors
  */
 
 // Config
@@ -11,21 +13,21 @@
 
 // Shared
 #include "shared/adc_utils.h"
-#include "shared/adc_utils.cpp"
+//#include "shared/adc_utils.cpp"
 #include "shared/ADS1118.h"
-#include "shared/ADS1118.cpp"
+//#include "shared/ADS1118.cpp"
 #include "shared/logger.h"
 #include "shared/voltage_utils.h"
-#include "shared/voltage_utils.cpp"
+//#include "shared/voltage_utils.cpp"
 
 // Charge
 #include "charge/charge.h"
-#include "charge/charge.cpp"
+//#include "charge/charge.cpp"
 
 // Discharge
 #include "discharge/discharge.h"
-#include "discharge/discharge.cpp"
+//#include "discharge/discharge.cpp"
 
 // Thermal
 #include "thermal/thermal.h"
-#include "thermal/thermal.cpp"
+//#include "thermal/thermal.cpp"
