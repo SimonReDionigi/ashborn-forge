bf45f7ba8673d0a599ce614fca7b1b3540d789c8
RiccardoCanalipv <riccardo.canali1@outlook.com>
2025-08-09 09:41:16 +0200

    Update feather_hub.cpp
    Removed config_protoA.h, is already included in config_current.h

--- firmware/feather_hub.cpp
+++ firmware/feather_hub.cpp
@@ -4,30 +4,24 @@
  * Author: Phoenix Volt
  * Created: 2025-08-04
  * Last Modified: 
- * - 2025-08-05 by Riccardo Canali - Commented .cpp files to avoid compilation errors
+ * - 2025-08-09: Removed config_protoA.h, is already included in config_current.h
+ * - 2025-08-05: Commented .cpp files to avoid compilation errors
  */
 
 // Config
 #include "config/config_current.h"
-#include "config/config_protoA.h"
 
 // Shared
 #include "shared/adc_utils.h"
-//#include "shared/adc_utils.cpp"
 #include "shared/ADS1118.h"
-//#include "shared/ADS1118.cpp"
 #include "shared/logger.h"
 #include "shared/voltage_utils.h"
-//#include "shared/voltage_utils.cpp"
 
 // Charge
 #include "charge/charge.h"
-//#include "charge/charge.cpp"
 
 // Discharge
 #include "discharge/discharge.h"
-//#include "discharge/discharge.cpp"
 
 // Thermal
 #include "thermal/thermal.h"
-//#include "thermal/thermal.cpp"
